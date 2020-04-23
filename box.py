2f3a27b52a99af0e1747b1e8c8ac15dcee3bd40a
Ramc26 <rakeshchowdary885@gmail.com>
2020-04-23 09:50:23 +0530

    python_turtle_module_files

---
+++ box.py
@@ -0,0 +1,25 @@
+import turtle
+import math
+ram=turtle.Turtle()
+ram.speed(100)
+for i in range(45):
+    ram.color("red","yellow")
+    """code to draw sperical cubes"""
+    ram.begin_fill()
+    ram.forward(100)
+    ram.right(90)
+    ram.forward(100)
+    ram.right(90)
+    ram.forward(100)
+    ram.right(90)
+    ram.forward(100)
+    ram.right(100)
+    ram.end_fill()
+####    ram.penup()
+####    ram.forward(10)
+####    ram.pendown()
+##    """code to draw sperical circles"""
+##    ram.circle(80)
+##    ram.right(10)
+
+        
---
+++ flower.py
@@ -0,0 +1,10 @@
+import turtle
+ram = turtle.Turtle()
+ram.speed(10)
+for i in range(36):
+    ram.color("red")
+    ram.forward(200)
+    ram.left(170)
+    ram.color("blue")
+    ram.forward(200)
+turtle.done()
